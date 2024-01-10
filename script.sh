ee7cad3eeb86da72c1b155f41e0eee3afb1625bc
Pokharel Bikarna <bikarnap@gmail.com>
2024-01-10 17:35:27 +0200

    Jenkins-Tutorial-SCM first commit

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "This is an example script" >> output.txt
+echo "It creates another script, which updates Debian based Ubuntu OS" >> output.txt
+
+echo "#!/bin/bash" >> update_ubuntu.sh
+echo "apt-get update" >> update_ubuntu.sh
