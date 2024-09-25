36b0d68938c1e74617d159678bf7bd44e62d3737
Bikarna Pokharel <15520600+bikarnap@users.noreply.github.com>
2024-09-25 20:03:13 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "This is an example script" | sed -a output.txt
-echo "It creates another script, which updates Debian based Ubuntu OS" | sed -a output.txt
+echo "This is an example script" | tee -a output.txt
+echo "It creates another script, which updates Debian based Ubuntu OS" | tee -a output.txt
 
-echo "#!/bin/bash" | sed -a update_ubuntu.sh
-echo "apt-get update" | sed -a update_ubuntu.sh
+echo "#!/bin/bash" | tee -a update_ubuntu.sh
+echo "apt-get update" | tee -a update_ubuntu.sh
